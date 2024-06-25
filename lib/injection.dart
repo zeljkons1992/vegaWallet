9002dab075c9c2dfe690c837d9419a13de034147
VojinBjelica <vojinb111@gmail.com>
2024-06-25 13:46:13 +0200

    Move some variables to .env

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:isar/isar.dart';
