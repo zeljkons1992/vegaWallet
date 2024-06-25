e1540895541fc7507228e7188cd2c306b051862c
VojinBjelica <vojinb111@gmail.com>
2024-06-25 13:46:13 +0200

    Move some variables to .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vegawallet/features/stores/presentation/screens/store_screen.dart';
-import 'features/stores/presentation/bloc/store_bloc.dart';
 import 'injection.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   configureDependencies();
 
   runApp(MyApp());
