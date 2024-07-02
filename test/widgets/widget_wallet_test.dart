aa6a9a53e7bb30dbfe2fb14abeff70646d7d368b
VojinBjelica <vojinb111@gmail.com>
2024-07-02 17:32:40 +0200

    Fix wallet test

--- test/widgets/widget_wallet_test.dart
+++ test/widgets/widget_wallet_test.dart
@@ -3,14 +3,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:bloc_test/bloc_test.dart';
+import 'package:vegawallet/features/stores/presentation/bloc/store_bloc.dart';
 import 'package:vegawallet/features/wallet/presentation/bloc/wallet_bloc.dart';
 import 'package:vegawallet/features/wallet/presentation/screens/wallet_screen.dart';
 import 'package:vegawallet/features/wallet/data/models/wallet_card_information.dart';
 
 class MockWalletBloc extends MockBloc<WalletEvent, WalletState> implements WalletBloc {}
+class MockStoreBloc extends MockBloc<StoreEvent, StoreState> implements StoreBloc {}
 
 void main() {
   late MockWalletBloc mockWalletBloc;
+  late MockStoreBloc mockStoreBloc;
 
   setUpAll(() {
     GetIt.I.reset();
@@ -18,26 +21,33 @@ void main() {
 
   setUp(() {
     mockWalletBloc = MockWalletBloc();
+    mockStoreBloc = MockStoreBloc();
     GetIt.I.registerFactory<WalletBloc>(() => mockWalletBloc);
+    GetIt.I.registerFactory<StoreBloc>(() => mockStoreBloc);
   });
 
   tearDown(() {
     GetIt.I.unregister<WalletBloc>();
+    GetIt.I.unregister<StoreBloc>();
   });
 
   Widget createWidgetUnderTest() {
     return MaterialApp(
-      home: BlocProvider.value(
-        value: mockWalletBloc,
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider<WalletBloc>(create: (_) => mockWalletBloc),
+          BlocProvider<StoreBloc>(create: (_) => mockStoreBloc),
+        ],
         child: const WalletScreen(),
       ),
     );
   }
 
-
   testWidgets('shows loaded card information when state is WalletStateLoaded', (WidgetTester tester) async {
     const mockCardInfo = WalletCardInformation(name: 'John Doe', expiry: '12/34', cardNo: '123456789012');
     whenListen(mockWalletBloc, Stream.fromIterable([const WalletStateLoaded(mockCardInfo)]), initialState: WalletStateInitial());
+    whenListen(mockStoreBloc, Stream.fromIterable([StoreLoaded(stores: [])]), initialState: StoreLoading());
+
     await tester.pumpWidget(createWidgetUnderTest());
     await tester.pumpAndSettle();
 
