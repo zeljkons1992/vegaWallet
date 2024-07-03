c999b252c1f084813f0c1895833f0a6891fbde67
nikolarankovic <nikolarankovic2810@gmail.com>
2024-07-03 17:40:08 +0200

    add some class

--- lib/features/stores/domain/repository/location_repository.dart
+++ lib/features/stores/domain/repository/location_repository.dart
@@ -1,5 +1,10 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:vegawallet/core/data_state/data_state.dart';
+
 import '../entities/position.dart';
 
+
 abstract class LocationRepository{
-  Future<Position> getCurrentLocation();
+  Future<DataState<Position>> getCurrentLocation();
+  Future<DataState<PositionSimple>> getLocationPickedStore(String address);
 }
