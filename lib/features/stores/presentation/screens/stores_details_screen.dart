f34b26b81bcea0cee560697aca659cc417200566
nikolarankovic <nikolarankovic2810@gmail.com>
2024-09-10 08:49:01 +0200

    fix back button no rerender

--- lib/features/stores/presentation/screens/stores_details_screen.dart
+++ lib/features/stores/presentation/screens/stores_details_screen.dart
@@ -20,7 +20,6 @@ import '../components/details_screen/maps/map_location_loaded.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../components/details_screen/maps/maps_unsuccessfully.dart';
-
 class StoreDetailsScreen extends StatefulWidget {
   final Store store;
 
@@ -82,174 +81,180 @@ class StoreDetailsScreenState extends State<StoreDetailsScreen> {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
     if (selectedDropdownItem != null) {
       addressCity =
-          "${selectedDropdownItem!.address}, ${selectedDropdownItem!.city}";
+      "${selectedDropdownItem!.address}, ${selectedDropdownItem!.city}";
     }
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) =>
-              _locationBloc..add(UpdateStoreLocation(addressCity ?? '')),
-        ),
 
-        BlocProvider(
-          create: (context) => _favoritesBloc..add(GetFavorites()),
-        )
-      ],
-      child: Scaffold(
-        body: Column(
-          children: [
-            AnimatedContainer(
-              duration: const Duration(milliseconds: 500),
-              curve: Curves.easeInOut,
-              height: isMapExpanded
-                  ? MediaQuery.of(context).size.height -
-                      MediaQuery.of(context).padding.top -
-                      SIZE_OF_BOTTOM_NAVIGATION_BAR
-                  : MediaQuery.of(context).size.height / 3,
-              child: Stack(
-                children: [
-                  BlocConsumer<LocationBloc, LocationState>(
-                    listener: (context, state) {
-                      switch (state) {
-                        case OpenNavigationToAddressUnsuccessful _:
-                          Fluttertoast.showToast(
-                            msg: localization.noFindAddress,
-                            toastLength: Toast.LENGTH_SHORT,
-                            gravity: ToastGravity.CENTER,
-                          );
-                      }
-                    },
-                    buildWhen: (previousState, currentState) {
-                      if (currentState
-                          is OpenNavigationToAddressUnsuccessful) {
-                        return false;
-                      }
-                      return true;
-                    },
-                    builder: (context, state) {
-                      switch (state) {
-                        case LocationInitial _:
-                          return mapLocationInitial(context);
-                        case FetchStoreLocationSuccessState _:
-                          return MapLocationLoadedWidget(
-                            shouldCenter: isMyCurrentLocationActive,
-                            latitude: state.position.latitude,
-                            longitude: state.position.longitude,
-                            zoomLevel: zoomLevel,
-                          );
-                        case FetchStoreLocationUnsuccessfullyState _:
-                          return const MapsUnsuccessfully();
-                        case LocationLoading _:
-                          return const Center(
-                              child: CircularProgressIndicator());
-                        case NoInternetConnectionState _:
-                          return const MapLocationError();
-                        default:
-                          return const SizedBox();
-                      }
-                    },
-                  ),
-                  PrimaryBackButton(
-                    onBackPressed: () {
-                      //context.push("/stores", extra: updatedStore);
-                      context.push("/stores");
-                    },
-                  ),
-                  Positioned(
-                    bottom: 16,
-                    left: 16,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.black.withOpacity(0.3),
-                      ),
-                      child: IconButton(
-                        onPressed: () => _expandMapAndShowLocations(),
-                        icon: Icon(
-                          isMapExpanded
-                              ? Icons.fullscreen_exit
-                              : Icons.fullscreen,
-                          color: Colors.white,
+    return PopScope(
+      canPop: true,
+      onPopInvoked:(didPop) {
+        context.push("/stores");
+      },
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) =>
+            _locationBloc..add(UpdateStoreLocation(addressCity ?? '')),
+          ),
+
+          BlocProvider(
+            create: (context) => _favoritesBloc..add(GetFavorites()),
+          )
+        ],
+        child: Scaffold(
+          body: Column(
+            children: [
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeInOut,
+                height: isMapExpanded
+                    ? MediaQuery.of(context).size.height -
+                    MediaQuery.of(context).padding.top -
+                    SIZE_OF_BOTTOM_NAVIGATION_BAR
+                    : MediaQuery.of(context).size.height / 3,
+                child: Stack(
+                  children: [
+                    BlocConsumer<LocationBloc, LocationState>(
+                      listener: (context, state) {
+                        switch (state) {
+                          case OpenNavigationToAddressUnsuccessful _:
+                            Fluttertoast.showToast(
+                              msg: localization.noFindAddress,
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.CENTER,
+                            );
+                        }
+                      },
+                      buildWhen: (previousState, currentState) {
+                        if (currentState
+                        is OpenNavigationToAddressUnsuccessful) {
+                          return false;
+                        }
+                        return true;
+                      },
+                      builder: (context, state) {
+                        switch (state) {
+                          case LocationInitial _:
+                            return mapLocationInitial(context);
+                          case FetchStoreLocationSuccessState _:
+                            return MapLocationLoadedWidget(
+                              shouldCenter: isMyCurrentLocationActive,
+                              latitude: state.position.latitude,
+                              longitude: state.position.longitude,
+                              zoomLevel: zoomLevel,
+                            );
+                          case FetchStoreLocationUnsuccessfullyState _:
+                            return const MapsUnsuccessfully();
+                          case LocationLoading _:
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          case NoInternetConnectionState _:
+                            return const MapLocationError();
+                          default:
+                            return const SizedBox();
+                        }
+                      },
+                    ),
+                    PrimaryBackButton(
+                      onBackPressed: () {
+                        Navigator.of(context).pop(); // Trigger back navigation
+                      },
+                    ),
+                    Positioned(
+                      bottom: 16,
+                      left: 16,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.black.withOpacity(0.3),
+                        ),
+                        child: IconButton(
+                          onPressed: () => _expandMapAndShowLocations(),
+                          icon: Icon(
+                            isMapExpanded
+                                ? Icons.fullscreen_exit
+                                : Icons.fullscreen,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            if (!isMapExpanded)
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 70,
-                        color: colorScheme.surfaceBright,
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Row(
-                          children: [
-                            Builder(
-                              builder: (context) {
-                                return PrimaryDropdownButton(
-                                  items: widget.store.addressCities,
-                                  selectedItem: selectedDropdownItem,
-                                  onChanged: (value) async {
-                                    setState(() {
-                                      selectedDropdownItem = value;
-                                    });
-
-                                    if (selectedDropdownItem != null) {
-                                      String addressCity =
-                                          "${selectedDropdownItem!.address}, ${selectedDropdownItem!.city}";
-                                      BlocProvider.of<LocationBloc>(context)
-                                          .add(UpdateStoreLocation(
-                                              addressCity));
-                                    }
-                                  },
-                                );
-                              },
-                            ),
-                            const SizedBox(width: 8),
-                            Container(
-                              height: 50,
-                              width: 50,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: colorScheme.primary,
-                              ),
-                              child: Builder(builder: (context) {
-                                return IconButton(
-                                  onPressed: () {
-                                    BlocProvider.of<LocationBloc>(context)
-                                        .add(
-                                      OpenNavigationToAddress(
-                                          "${selectedDropdownItem!.address}, ${selectedDropdownItem!.city}"),
-                                    );
-                                  },
-                                  icon: const Icon(
-                                    Icons.directions,
-                                  ),
-                                  color: Colors.white,
-                                );
-                              }),
-                            ),
-                          ],
-                        ),
-                      ),
-                      ItemDetailsInfo(
-                        store: widget.store,
-                        onUpdate: (Store updated) {
-                          setState(() {
-                            updatedStore = updated;
-                          });
-                        },
-                      ),
-                    ],
-                  ),
+                  ],
                 ),
               ),
-          ],
+              if (!isMapExpanded)
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 70,
+                          color: colorScheme.surfaceBright,
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Row(
+                            children: [
+                              Builder(
+                                builder: (context) {
+                                  return PrimaryDropdownButton(
+                                    items: widget.store.addressCities,
+                                    selectedItem: selectedDropdownItem,
+                                    onChanged: (value) async {
+                                      setState(() {
+                                        selectedDropdownItem = value;
+                                      });
+
+                                      if (selectedDropdownItem != null) {
+                                        String addressCity =
+                                            "${selectedDropdownItem!.address}, ${selectedDropdownItem!.city}";
+                                        BlocProvider.of<LocationBloc>(context)
+                                            .add(UpdateStoreLocation(
+                                            addressCity));
+                                      }
+                                    },
+                                  );
+                                },
+                              ),
+                              const SizedBox(width: 8),
+                              Container(
+                                height: 50,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: colorScheme.primary,
+                                ),
+                                child: Builder(builder: (context) {
+                                  return IconButton(
+                                    onPressed: () {
+                                      BlocProvider.of<LocationBloc>(context)
+                                          .add(
+                                        OpenNavigationToAddress(
+                                            "${selectedDropdownItem!.address}, ${selectedDropdownItem!.city}"),
+                                      );
+                                    },
+                                    icon: const Icon(
+                                      Icons.directions,
+                                    ),
+                                    color: Colors.white,
+                                  );
+                                }),
+                              ),
+                            ],
+                          ),
+                        ),
+                        ItemDetailsInfo(
+                          store: widget.store,
+                          onUpdate: (Store updated) {
+                            setState(() {
+                              updatedStore = updated;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
